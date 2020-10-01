f74e26d3b5dda93a5af27c357d8979fa4d65c5fd
Yuri Mussi <ymussi@gmail.com>
2020-10-01 16:49:30 -0300

    react foundations

---
+++ src/components/basics/Primeiro.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Primeiro() {
+    return <h2>
+        Primeiro Componente
+    </h2>
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reactDOM from 'react-dom';
+import React from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Primeiro from './components/basics/Primeiro'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const el = document.getElementById('root');
+// const hello = <strong>Olá React!!</strong>;
+
+
+reactDOM.render(
+    <div>
+        {/* { hello } */}
+        <Primeiro></Primeiro>
+    </div>,
+    el
+    );

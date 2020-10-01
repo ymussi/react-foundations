1c1e4f119a179e698fc5db11030f67c492fec0bc
Yuri Mussi <ymussi@gmail.com>
2020-10-01 16:51:40 -0300

    remove comment lines

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import React from 'react';
 import Primeiro from './components/basics/Primeiro'
 
 const el = document.getElementById('root');
-// const hello = <strong>Olá React!!</strong>;
-
 
 reactDOM.render(
     <div>
-        {/* { hello } */}
         <Primeiro></Primeiro>
     </div>,
     el

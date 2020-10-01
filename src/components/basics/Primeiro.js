2419ecb93026ee4b7d844e13a019a23fa67caaa5
Yuri Mussi <ymussi@gmail.com>
2020-10-01 16:56:38 -0300

    interpretando variavel dentro do jsx

--- src/components/basics/Primeiro.js
+++ src/components/basics/Primeiro.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 export default function Primeiro() {
-    return <h2>
-        Primeiro Componente
-    </h2>
+    const msg = "Seja bem vindo(a)!"
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{ msg }</p>
+        </div>
+    )
 };
